5846793df09e80cd9bc9cabfec43203cbc37d4bc
deokgeneral <alsejrrl@naver.com>
2024-02-13 16:42:51 +0900

    1~9강과제

---
+++ screens/Mainscreen.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+
+const Mainscreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>ToDo App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Mainscreen
+
+const styles = StyleSheet.create({})
